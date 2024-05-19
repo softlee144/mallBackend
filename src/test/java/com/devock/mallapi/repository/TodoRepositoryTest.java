58be21de6103d0023dfb5c68f8a5873181f4fb18
softlee144 <ockjungbong@gmail.com>
2024-05-19 22:15:44 +0900

    페이징 테스트

--- src/test/java/com/devock/mallapi/repository/TodoRepositoryTest.java
+++ src/test/java/com/devock/mallapi/repository/TodoRepositoryTest.java
@@ -6,6 +6,10 @@ import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import com.devock.mallapi.domain.Todo;
 
@@ -64,4 +68,11 @@ public class TodoRepositoryTest {
         todoRepository.deleteById(tno);
     }
     
+    @Test 
+    public void testPaging() {
+        Pageable pageable = PageRequest.of(0,10, Sort.by("tno").descending());
+        Page<Todo> result = todoRepository.findAll(pageable);
+        log.info(result.getTotalElements());
+        result.getContent().stream().forEach(todo -> log.info(todo));
+    }
 }
