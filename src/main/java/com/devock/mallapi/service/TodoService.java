71f3a218b4d52c0456849541d300f9bd66f02968
softlee144 <ockjungbong@gmail.com>
2024-05-19 22:44:15 +0900

    서비스 선언

---
+++ src/main/java/com/devock/mallapi/service/TodoService.java
@@ -0,0 +1,7 @@
+package com.devock.mallapi.service;
+
+import com.devock.mallapi.dto.TodoDTO;
+
+public interface TodoService {
+    Long register(TodoDTO todoDTO);
+}
---
+++ src/main/java/com/devock/mallapi/service/TodoServiceImpl.java
@@ -0,0 +1,20 @@
+package com.devock.mallapi.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devock.mallapi.dto.TodoDTO;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Transactional
+@Log4j2
+public class TodoServiceImpl implements TodoService {
+    @Override 
+    public Long register(TodoDTO todoDTO) {
+        log.info("............");
+        return null;
+    }
+    
+}
