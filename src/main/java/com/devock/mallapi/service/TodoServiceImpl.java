185dac3167ba746e8b9b23e13eb65977ef6b79c9
softlee144 <ockjungbong@gmail.com>
2024-05-20 23:27:51 +0900

    등록 기능 구현

--- src/main/java/com/devock/mallapi/service/TodoServiceImpl.java
+++ src/main/java/com/devock/mallapi/service/TodoServiceImpl.java
@@ -1,20 +1,35 @@
 package com.devock.mallapi.service;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devock.mallapi.domain.Todo;
 import com.devock.mallapi.dto.TodoDTO;
+import com.devock.mallapi.repository.TodoRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Service
 @Transactional
 @Log4j2
+@RequiredArgsConstructor // 생성자 자동 주입
 public class TodoServiceImpl implements TodoService {
+
+    // 자동 주입 대상은 final로
+    private final ModelMapper modelMapper;
+
+    private final TodoRepository todoRepository;
+
     @Override 
     public Long register(TodoDTO todoDTO) {
         log.info("............");
-        return null;
+
+        Todo todo = modelMapper.map(todoDTO, Todo.class);
+        Todo savedTodo = todoRepository.save(todo);
+        
+        return savedTodo.getTno();
     }
     
 }
---
+++ src/test/java/com/devock/mallapi/service/TodoServiceTests.java
@@ -0,0 +1,32 @@
+package com.devock.mallapi.service;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.devock.mallapi.dto.TodoDTO;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
+public class TodoServiceTests {
+
+    @Autowired
+    private TodoService todoService;
+
+    @Test
+    public void testRegister() {
+        TodoDTO todoDTO= TodoDTO.builder()
+        .title("서비스 테스트")
+        .writer("tester")
+        .dueDate(LocalDate.of(2023, 10, 10))
+        .build();
+
+        Long tno = todoService.register(todoDTO);
+        log.info("TNO : " + tno);
+    }
+    
+}
