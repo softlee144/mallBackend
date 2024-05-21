6d6ae1c3e9f379a7c1536b16cab475e36426ffa2
softlee144 <ockjungbong@gmail.com>
2024-05-22 00:02:34 +0900

    CRUD 처리 및 CORS 설정

--- src/main/java/com/devock/mallapi/config/CustomServletConfig.java
+++ src/main/java/com/devock/mallapi/config/CustomServletConfig.java
@@ -2,16 +2,32 @@ package com.devock.mallapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.devock.mallapi.controller.formatter.LocalDateFormatter;
 
 @Configuration
-public class CustomServletConfig implements WebMvcConfigurer{
+public class CustomServletConfig implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatter(new LocalDateFormatter());
     }
-    
+
+    /*
+     * CORS 설정
+     * Ajax를 이용해서 서비스를 호출하게 되면 반드시 '교차 출처 리소스 공유'로 인해 정상적 호출이 제한된다.
+     * 리액트에서 스프링 부트로 동작하는 서버를 호출하기 위해서 아래 설정이 필요하다.
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(300)
+                .allowedHeaders("Authorization", "Cache-Control", "Content-Type");
+    }
+
 }
--- src/main/java/com/devock/mallapi/controller/TodoController.java
+++ src/main/java/com/devock/mallapi/controller/TodoController.java
@@ -1,5 +1,13 @@
 package com.devock.mallapi.controller;
 
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,10 +18,6 @@ import com.devock.mallapi.service.TodoService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -21,21 +25,52 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/api/todo")
 public class TodoController {
 
-    private final TodoService todoService;
+    private final TodoService service;
 
     @GetMapping("/{tno}")
-    public TodoDTO get(@PathVariable(name="tno") Long tno) {
-        return todoService.get(tno);
+    public TodoDTO get(@PathVariable(name = "tno") Long tno) {
+        return service.get(tno);
     }
 
     @GetMapping("/list")
     public PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO) {
         log.info(pageRequestDTO);
 
-        return todoService.list(pageRequestDTO);
+        return service.list(pageRequestDTO);
     }
 
+    @PostMapping("/")
+    public Map<String, Long> register(@RequestBody TodoDTO todoDTO) {
 
-    
-    
+        log.info("TodoDTO: " + todoDTO);
+
+        Long tno = service.register(todoDTO);
+
+        return Map.of("TNO", tno);
+    }
+
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(
+            @PathVariable(name = "tno") Long tno,
+            @RequestBody TodoDTO todoDTO) {
+
+        todoDTO.setTno(tno);
+
+        log.info("Modify: " + todoDTO);
+
+        service.modify(todoDTO);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable(name = "tno") Long tno) {
+
+        log.info("Remove:  " + tno);
+
+        service.remove(tno);
+
+        return Map.of("RESULT", "SUCCESS");
+    }
+
 }
